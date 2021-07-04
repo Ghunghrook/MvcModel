b3a8281a4f8f5cfdc80f4cae3ab0d8015c7c62fa
ghunghrook <jolly.89@gmail.com>
2021-07-04 20:35:12 +0530

    iniail commit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcModel.Models;
+
+namespace MvcModel.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+
+        public ActionResult Index(int departmentId)
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+            List<Employee> employees = employeeContext.Employees.Where(e => e.DepartmentId == departmentId).ToList();
+
+            return View(employees);
+        }
+
+        public ActionResult Details(int id)
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+            Employee employee =  employeeContext.Employees.Single(emp => emp.Id == id);
+
+            //Employee employee = new Employee()
+            //{
+            //    EmployeeId = 101,
+            //    FirstName = "ghun",
+            //    LastName = "khare",
+            //    Gender = "female",
+            //    Salary = 10000
+            //};
+
+            return View(employee);
+        }
+    }
+}
